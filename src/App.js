2bff73c6c14041ef29d2c06897dc4f53aadffbe2
akash kakde <akashkakde1996@gmail.com>
2022-05-27 11:29:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css";
+//import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap";
 import './App.css';
+import './component/style.css';
+import Withcontext from './component/withcontext';
+import { BrowserRouter , Navigate, Route , Routes } from "react-router-dom";
+//import { BrowserRouter , Navigate, Route , Routes } from "react-dom/client";
+import Login  from './component/Login';
+import { Products } from './component/Products';
+import Cart from './component/Cart';
+import ProductDetail from './component/ProductDetail';
+import Checkoutform from './component/checkoutform';
+import { Register } from './component/register';
+import { LoginPage } from './component/LoginPage';
 
-function App() {
+const App = () => {
+  const flag=localStorage.getItem("flag");
+  
+  console.log(flag)
+  let display;
+  if(flag==="true"){
+    display =  ""
+  }
+  else{
+    display = "incorrect data";
+  }
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Withcontext>
+      <Routes>
+        {/*<Route exact path="/" element={<Products/>}/>*/}
+        {<Route exact path="/" element={<Login/>}/>}
+        <Route  path="/login" element={<Login/>}/>
+
+        <Route  path="/loginpage" element={<LoginPage/>}/>
+
+        <Route path="/register" element={<Register/>}/>
+
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/products/:id" element={<ProductDetail/>}/>
+
+        <Route exact path="/cart" element={<Cart/>}/>
+        <Route path="/cart/:id" element={<ProductDetail/>}/>
+
+        <Route path="/checkoutform" element={<Checkoutform/>}/>
+
+      </Routes>
+      </Withcontext>  
+    </BrowserRouter>
+  )
 }
 
 export default App;
